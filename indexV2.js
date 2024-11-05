27bedab20beea74c65887443813269a8e9fe0fb7
Hydration2O <bomalk121@gmail.com>
2024-11-05 18:31:49 +0300

    V2 is proper

--- indexV2.js
+++ indexV2.js
@@ -18,15 +18,14 @@ Task 2) Create a new array containing only the expense transactions.
        Output 2) [["expense", 500], ["expense", 300]]
 
 ******************************************************************/
-let incomeArr=[];
-let expenseArr=[];
-for (let i=0;i<transactions.length;i++){
-       if (transactions[i][0]==="income") incomeArr.push(transactions[i]);
-       else expenseArr.push(transactions[i]);
-}
-console.log(`${transactions[0][0]}`);
-console.log(`${incomeArr}`);
-console.log(`${expenseArr}`);
+// function printIncome(){
+//    return transactions.filter((income)=>income[0]==="income"); //income[0] == transactions[i][0]
+// }
+const incomeStatement=transactions.filter(elem=>elem[0] ==="income");
+const expenseStatement=transactions.filter(elem=>elem[0] ==="expense");
+console.log(incomeStatement);
+console.log(expenseStatement);
+
 
 /*****************************************************************
 Part 2: Financial Summary
@@ -38,12 +37,11 @@ Task 4) Calculate the total expenses and return it.
        Output 4) 800
 
 ******************************************************************/
- let totalIncome=0;
- let totalExpense=0;
-for (let i=0;i<incomeArr.length;i++) totalIncome=totalIncome+incomeArr[i][1];
-for (let i=0;i<expenseArr.length;i++) totalExpense=totalExpense+expenseArr[i][1];
-console.log(`${totalIncome}`);
-console.log(`${totalExpense}`);
+const totalIncome = incomeStatement.map((elem)=>(elem=elem[1])).reduce((total,elem)=>total+=elem);
+const totalExpense = expenseStatement.map((elem)=>(elem=elem[1])).reduce((total,elem)=>total+=elem);
+console.log(totalIncome);
+console.log(totalExpense);
+
 
 /*****************************************************************
 Part 3: Net Analysis
@@ -55,8 +53,12 @@ Task 6) Identify and create a new array with transactions (both incomes and expe
        Output 6) [["income", 1000], ["income", 1500], ["expense", 500], ["income", 700]]
 
 ******************************************************************/
-let netTotal= totalIncome-totalExpense;
-console.log(`${netTotal}`);
+let netProfit=totalIncome-totalExpense;
+console.log(netProfit);
 
-function above500(money)
+const above500array = transactions.filter((elem)=>(elem[1]>=500));
+console.log(above500array);
+
+
+
 // Note: Use appropriate JavaScript array iteration methods such as filter, reduce, etc., to achieve the desired output for each task.
